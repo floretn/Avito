c3b2562d93de052725d1b4ff8bd6ebb39cefdb04
floretn <ivanse99@yandex.ru>
2021-02-17 04:06:59 +0300

    changes in sql-script

--- schema_DB.sql
+++ schema_DB.sql
@@ -1,23 +1,26 @@
 ﻿begin;
 
---drop schema if exists investor_workspace cascade;
+--drop schema if exists client_workspace cascade;
 create schema client_workspace;
 
-CREATE TABLE client_workspace.t_applications
+CREATE TABLE client_workspace.t_ads
 ( 
-	app_pk		serial, -- id
-	app_name	character varying (200) not null, -- Название
-	app_dsc		character varying (1000) not null, -- Описание
-	PRIMARY KEY (app_pk)
+	ad_pk		serial, -- id
+	ad_name		character varying (200) not null, -- Название
+	ad_price	numeric not null, --Цена
+	ad_dsc		character varying (1000) not null, -- Описание
+	ad_date		timestamp, --Дата создания объявления
+	PRIMARY KEY (ad_pk)
 );
 
 CREATE TABLE client_workspace.t_links
 ( 
 	lnk_pk			serial, -- id
-	lnk_app_fk		integer, -- Ссылка на заявку
+	lnk_ad_fk		integer, -- Ссылка на объявление
 	lnk_value		character varying (255) not null, -- Значение ссылки
+	lnk_gen			boolean, -- Главное фото или нет
 	PRIMARY KEY (lnk_pk),
-	FOREIGN KEY (lnk_app_fk) REFERENCES client_workspace.t_applications (app_pk)
+	FOREIGN KEY (lnk_ad_fk) REFERENCES client_workspace.t_ads (ad_pk)
 );
 
 end;
